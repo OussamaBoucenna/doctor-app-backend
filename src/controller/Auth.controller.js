3a8190965222456aa887c291846ed72f4ee1677e
OUSSAMA's Pc <lo_boucenna@esi.dz>
2025-04-24 17:20:13 +0100

    nothing just consolog

--- src/controller/Auth.controller.js
+++ src/controller/Auth.controller.js
@@ -5,6 +5,8 @@ exports.register = async (req, res) => {
     const { firstName, lastName, email, password, phone, role } = req.body;
     const image = req.file ? req.file.buffer : null; // Image en binaire
 
+    console.log('Image:', image); // Debugging
+    console.log('Request Body:', req.body); // Debugging
     const user = await authService.register({
       firstName,
       lastName,
@@ -18,6 +20,7 @@ exports.register = async (req, res) => {
     res.status(201).json(user);
   } catch (err) {
     res.status(400).json({ error: err.message });
+    console.log('Error:', err.message); // Debugging
   }
 };
 
