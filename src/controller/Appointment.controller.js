3dda14e4a0ddd0d27860fc07ad0a193abf543a18
Oussama BOUCENNA <lo_boucenna@esi.dz>
2025-05-09 01:03:58 +0100

    next appointment of doctor added

--- src/controller/Appointment.controller.js
+++ src/controller/Appointment.controller.js
@@ -1,5 +1,11 @@
 // controllers/appointment.controller.js
 const appointmentService = require('../service/Appointment.service'); 
+const Appointment = require('../model/Appointment.model');
+const AppointmentSlot = require('../model/AppointmentSlot.model');
+const DoctorSchedule = require('./../model/DoctorSchedule.model');
+const Patient = require('../model/Patient.model');
+const User = require('../model/User.model');
+const { Op } = require('sequelize');
 
 const create = async (req, res) => {
   try {
@@ -93,7 +99,117 @@ const remove = async (req, res) => {
 
 
 
+
+const getNextAppointment = async (req, res) => {
+  try {
+    const  doctorId = req.doctorId;
+    
+    // Vérifier si le docteur existe
+    if (!doctorId) {
+      return res.status(400).json({
+        success: false,
+        message: 'L\'identifiant du médecin est requis'
+      });
+    }
+    
+   
+
+    
+    const currentDate = new Date();
+    
+  
+    
+    
+    const nextAppointment = await Appointment.findOne({
+      include: [
+        {
+          model: AppointmentSlot,
+          // as: appointmentSlotAlias,
+          where: {
+            // Vérifier que la date et l'heure sont dans le futur
+            [Op.or]: [
+              {
+                working_date: {
+                  [Op.gt]: currentDate.toISOString().split('T')[0] // Date actuelle
+                }
+              },
+              {
+                working_date: currentDate.toISOString().split('T')[0],
+                start_time: {
+                  [Op.gt]: currentDate.toTimeString().split(' ')[0] // Heure actuelle
+                }
+              }
+            ]
+          },
+          include: [
+            {
+              model: DoctorSchedule,
+              where: { doctor_id: doctorId }
+            }
+          ]
+        },
+        {
+          model: Patient,
+          // as: patientAlias,
+          include: [
+            {
+              model: User,
+              attributes: ['first_name', 'last_name', 'email', 'phone']
+            }
+          ]
+        }
+      ],
+      where: {
+        status: {
+          [Op.in]: ['PENDING', 'CONFIRMED']
+        }
+      },
+      order: [
+        [AppointmentSlot, 'working_date', 'ASC'],
+        [ AppointmentSlot,'start_time', 'ASC']
+      ],
+      limit: 1,
+    });
+
+    if (!nextAppointment) {
+      return res.status(404).json({
+        success: false,
+        message: 'Aucun rendez-vous à venir pour ce médecin'
+      });
+    }
+    const count = await appointmentService.countAppointmentsForPatientAndDoctor(nextAppointment.patient_id, doctorId);
+    console.log('Nombre de visites pour le patient et le médecin:', nextAppointment.PATIENT);
+    return res.status(200).json({
+      success: true,
+      message: 'Prochain rendez-vous récupéré avec succès',
+      nextAppointment: {
+        appointement_id : nextAppointment.appointment_id,
+        patient_id: nextAppointment.patient_id,
+        status: nextAppointment.status,  
+        fullname : `${nextAppointment.PATIENT.USER.first_name} ${nextAppointment.PATIENT.USER.last_name}`,
+        start_time: nextAppointment.APPOINTMENT_SLOT.start_time,
+        reason : nextAppointment.reason,
+        numberOfVisit : count
+      }
+    });
+  } catch (error) {
+    console.error('Erreur lors de la récupération du prochain rendez-vous:', error);
+    return res.status(500).json({
+      success: false,
+      message: 'Erreur lors de la récupération du prochain rendez-vous',
+      error: error.message
+    });
+  }
+};
+
+
+
+
+
+
+
 module.exports = {
+  getNextAppointment,
   create,
   getAll,
   getAppointmentsByPatientId,
--- src/routes/Appointment.routes.js
+++ src/routes/Appointment.routes.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/Appointment.controller');
-const authMiddleware = require('../middlewares/Auth');
+const {authMiddleware,getDoctor} = require('../middlewares/Auth');
 
 router.post('/', controller.create);
 router.get('/', controller.getAll);
 router.get('/patient/',authMiddleware ,controller.getAppointmentsByPatientId);
+router.get('/doctor/my-next-appointments', authMiddleware, getDoctor , controller.getNextAppointment);
 router.get('/first-upcoming', authMiddleware, controller.getFirstUpcomingAppointmentByPatientId);
 router.get('/:id', controller.getAppointment);
 router.put('/:id', controller.update);
