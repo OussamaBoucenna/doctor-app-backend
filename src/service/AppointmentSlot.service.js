22462a463d17dfdd0886e79310167f7740366464
thanina-amrt <lt_amirat@esi.dz>
2025-05-18 21:20:33 +0100

    favourite backend

--- src/service/AppointmentSlot.service.js
+++ src/service/AppointmentSlot.service.js
@@ -1,5 +1,5 @@
 const AppointmentSlot = require('../model/AppointmentSlot.model');
-const DoctorSchedule = require('../model/DoctorSchedule.model');
+const DoctorSchedule = require('../model/doctorSchedule.model');
 const Doctor = require('../model/Doctor.model');
 const { sequelize } = require('../config/config');
 const { Op } = require('sequelize');
