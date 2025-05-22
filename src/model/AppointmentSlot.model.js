920754ba8be189035d195a51363b758baa169b19
Oussama BOUCENNA <lo_boucenna@esi.dz>
2025-05-22 09:42:07 +0100

    skjfsd

--- src/model/AppointmentSlot.model.js
+++ src/model/AppointmentSlot.model.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/config');
-const DoctorSchedule = require('./DoctorSchedule.model');
+const DoctorSchedule = require('./doctorSchedule.model');
 
 const AppointmentSlot = sequelize.define('APPOINTMENT_SLOTS', {
   slot_id: {
